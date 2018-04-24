41ba5a47566d8c3a89e3129cc236e3fcd1dfb3d8
shafin rahman <shafinr23@gmail.com>
2018-04-24 17:38:44 +0600

    splite files

--- App.js
+++ App.js
@@ -1,60 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View , TextInput , Button} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
 
-import ListItem from './src/components/ListItem/ListItem';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 
-export default class App extends React.Component {
+export default class App extends Component {
   state = {
-    placeName: "",
     places: []
   };
 
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName:val
-    });
-  };
-
-  placeSubmiHandler = ()=>{
-    if(this.state.placeName.trim() === ""){
-      return;
-    }
-    this.setState(prevState =>{
-      return{
-        places:prevState.places.concat(prevState.placeName)
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
       };
     });
   };
- 
-  
+
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-
-        <View style={styles.inputContainer} >
-        <TextInput 
-        style={styles.placeInput}
-        placeholder="search place"
-        value={this.state.placeName}
-        onChangeText={this.placeNameChangeHandler}
-        />
-
-        <Button 
-       style={styles.placeButton}
-        title="awesome"
-        onPress={this.placeSubmiHandler}
-        />
-        </View>
-        <View style={styles.placeList} >
-          {placesOutput}
-        </View>
-        
-            
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -62,27 +31,10 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  padding:10,
-  backgroundColor: 'transparent',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-  },
-  inputContainer:{
-   // flex:1,
-    width:"100%",
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center"
-  },
-  placeInput:{
-    width:"70%"
-  },
-  placeButton:{
-    width:"30%",
-  },
-  placeList:{
-    width:"100%"
+    flex: 1,
+    padding:10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
-  
 });
