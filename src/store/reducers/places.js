25db3615783ac028f0415de8af54eef9d2dac0aa
shafin rahman <shafinr23@gmail.com>
2018-04-29 19:22:35 +0600

    add redux

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialState ={
+    places: [],
+    selectPlace: null
+};
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+export default reducer
