2f764d433fb9c9ba61ac3819bb547369cd456554
shafin rahman <shafinr23@gmail.com>
2018-05-03 15:30:37 +0600

    add input color

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -11,9 +11,11 @@ class AuthScreen extends Component{
                 <Text>please login </Text>
 
                 <Button title="switch to login"/>
-                    <TextInput placeholder="your email address" />
-                    <TextInput placeholder="your password" />
-                    <TextInput placeholder="confirm password" />
+                <View style={styles.inputContainer} >
+                    <TextInput style={styles.input} placeholder="your email address" underlineColorAndroid="transparent" />
+                    <TextInput style={styles.input} placeholder="your password" underlineColorAndroid="transparent"/>
+                    <TextInput style={styles.input} placeholder="confirm password" underlineColorAndroid="transparent" />
+                </View>
                 <Button title="login" onPress={this.loginHandeler}/>
             </View>
         );
@@ -25,6 +27,16 @@ const styles =StyleSheet.create({
             flex:1,
             justifyContent:"center",
             alignItems:"center"
+        },
+        inputContainer:{
+            width:"80%"
+        },
+        input:{
+            width:"100%",
+            borderWidth:1,
+            borderColor:"#000",
+            padding:5,
+            margin:8
         }
 });
 
